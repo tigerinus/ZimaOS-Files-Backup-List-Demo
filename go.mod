1b2a2f1424088735cd77084168ddd5db461326e6
Tiger Wang <tigerwang@outlook.com>
2024-04-23 17:26:29 +0800

    wip

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/tigerinus/ZimaOS-Files-Backup-List-Demo
+
+go 1.21.8
+
+require (
+	github.com/IceWhaleTech/ZimaOS-Common v1.1.1-alpha2.0.20240423091120-157dd16e3fb0
+	github.com/oapi-codegen/runtime v1.1.1
+)
+
+require (
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/google/uuid v1.5.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,25 @@
+//go:generate bash -c "mkdir -p codegen/filesbackup && go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen@v2.1.0 -generate types,client -package filesbackup https://raw.githubusercontent.com/IceWhaleTech/IceWhale-OpenAPI/main/icewhale-files-backup/openapi.yaml > codegen/filesbackup/api.go"
+
+package main
+
+import (
+	"fmt"
+	"log"
+
+	commonFilesbackup "github.com/IceWhaleTech/ZimaOS-Common/filesbackup"
+	"github.com/tigerinus/ZimaOS-Files-Backup-List-Demo/codegen/filesbackup"
+)
+
+func main() {
+	metadataPath := commonFilesbackup.DefaultMetadataPath("/var/lib/icewhale")
+	allBackups, err := commonFilesbackup.GetAllBackups[filesbackup.FolderBackup](metadataPath)
+	if err != nil {
+		log.Fatalf("Error getting all backups: %v", err)
+	}
+
+	for _, backups := range allBackups {
+		for _, backup := range backups {
+			fmt.Println(backup.BackupFolderPath)
+		}
+	}
+}
